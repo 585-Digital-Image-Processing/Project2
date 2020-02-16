4b23496bcc438deb2aabf9a9702e01793a51043f
hul419 <hul419@psu.edu>
2020-02-16 14:14:27 -0500

    se

--- structure_E.m
+++ structure_E.m
@@ -1,4 +1,5 @@
 function [B_f, B_b] = Structure_E()
+% create a cell for foreground structuring elements
 B_f={};
 B_f{1} = [0 0 0; 0 1 0; 1 1 1];
 B_f{2}= [0 0 0 ;1 1 0; 1 1 0];
@@ -8,7 +9,8 @@ B_f{5} = [1 1 1 ; 0 1 0 ; 0 0 0 ];
 B_f{6} = [0 1 1 ; 0 1 1 ; 0 0 0 ];
 B_f{7} = [0 0 1; 0 1 1; 0 0 1];
 B_f{8} = [0 0 0 ; 0 1 1 ; 0 1 1];
- 
+
+ % create a cell for background structuring elements
 B_b={};
 B_b{1} = [1 1 1; 0 0 0; 0 0 0 ];
 B_b{2} = [0 1 1; 0 0 1; 0 0 0];
