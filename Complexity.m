cb70c2683368da52fea7338a8985e51fa02ea53b
jiuchao <jiuchao1@gmail.com>
2020-02-16 19:27:24 -0500

    update

--- Complexity.m
+++ Complexity.m
@@ -1,8 +1,30 @@
+%%%%%%%%%%%%%  Function Pectrum %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Purpose:  
+%      Compute the complexity of an image
+%      
+% Input Variables:
+%      f       Pectrum of the image, f(i) represent f_(i-1) because matlab
+%      is one-indexed
+%      
+% Returned Results:
+%      a       a complexity score
+%
+% Processing Flow:
+%      a = -sum of f(i)*log(f(i))
+%
+%  Restrictions/Notes:
+%     f must be a column vector
+%      
+%  The following functions are called:
+%      none
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function a = Complexity(f)
     r = length(f);
     a = 0;
     for i = 1:r
+        % Omitted f(i) = 0
         if(f(i) ~= 0)
             a = a - f(i)*log(f(i));
         end
--- sizeDistribution.m
+++ sizeDistribution.m
@@ -1,3 +1,25 @@
+%%%%%%%%%%%%%  Function Pectrum %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Purpose:  
+%      Compute the size distribution of an image
+%      
+% Input Variables:
+%      u       Mx1 size distribution of the image
+%      
+% Returned Results:
+%      f       Pectrum of the image, f(i) represent f_(i-1) because matlab
+%      is one-indexed
+%
+% Processing Flow:
+%      1. calculate f(i) = (U(i) - U(i+1))/U(1). Note that U(1) is the
+%      total area of the image of interest
+%
+%  Restrictions/Notes:
+%     U must be a column vector
+%      
+%  The following functions are called:
+%      none
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function  [u, r]  = sizeDistribution (img)
 
 % structuring element
